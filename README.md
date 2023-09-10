b37df5845150890691afe725298f5ced1ea925f9
SergiyPysanenko <SergiyPysanenko@gmail.com>
2023-09-10 17:25:07 +0300

    add hw 01

---
+++ README.md
@@ -0,0 +1,2 @@
+https://monosnap.com/file/k7H9G0gTLnCeXk6TZFJ43UAVRo5pvP
+https://monosnap.com/file/qeMgZjpxISAvGfaDN39IujLjYYuqD3
--- contacts.js
+++ contacts.js
@@ -1,31 +1,29 @@
 const fs = require('fs/promises');
 const path = require('path');
-const nanoid = require('nanoid');
+const { nanoid } = require('nanoid');
 
 const contactsPath = path.join(__dirname, 'db', 'contacts.json');
 console.log(contactsPath);
 
 const listContacts = async () => {
-    // ...твій код. Повертає масив контактів.
     const data = await fs.readFile(contactsPath);
     return JSON.parse(data);
   };
   console.log(listContacts);
   
   const getContactById = async id => {
-    // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
     const contacts = await listContacts();
     return contacts.find(contact => contact.id === id) || null;
   };
   console.log(getContactById);
   
   const addContact = async data => {
-    // ...твій код. Повертає об'єкт доданого контакту.
     const contacts = await listContacts();
     const newContact = {
       id: nanoid(),
       ...data,
     };
+
     contacts.push(newContact);
     await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
     return newContact;
@@ -33,7 +31,6 @@ const listContacts = async () => {
   console.log(addContact);
   
   const removeContact = async id => {
-    // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
     const contacts = await listContacts();
     const index = contacts.findIndex(contact => contact.id === id);
     if (index === -1) {
